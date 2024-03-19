eb64fde89d192108aff1e6aba289daa3c0e2a4f7
khalidlotfi <khalidlotfi2005@gmail.com>
2024-03-19 07:54:05 +0000

    fixing bootstrap @import

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import Header from './components/Header'
 import Content from './components/Content'
 import Footer from './components/Footer'
+
+
+// Import our custom CSS
+import './scss/styles.scss'
+
 import * as bootstrap from 'bootstrap'
 import Alert from 'bootstrap/js/dist/alert';
 
