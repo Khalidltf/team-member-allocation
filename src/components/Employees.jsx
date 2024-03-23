d9e888811d65a4826bdf2e3344777690bc4581b5
khalidlotfi <khalidlotfi2005@gmail.com>
2024-03-23 23:23:57 +0000

    + dropdown

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,109 +1,139 @@
-import {useState} from 'react'
-import maleProfile from '../assets/maleProfile.jpeg'
-import femaleProfile from '../assets/femaleProfile.jpeg'
-
+import { useState } from "react";
+import maleProfile from "../assets/maleProfile.jpeg";
+import femaleProfile from "../assets/femaleProfile.jpeg";
 
 const Employees = () => {
-
-  const [employees, setEmployees] = useState([{
-    id: 1,
-    fullName: "Bob Jones",
-    designation: "JavaScript Developer",
-    gender: "male",
-    teamName: "TeamA"
-  },
-  {
-    id: 2,
-    fullName: "Jill Bailey",
-    designation: "Node Developer",
-    gender: "female",
-    teamName: "TeamA"
-  },
-  {
-    id: 3,
-    fullName: "Gail Shepherd",
-    designation: "Java Developer",
-    gender: "female",
-    teamName: "TeamA"
-  },
-  {
-    id: 4,
-    fullName: "Sam Reynolds",
-    designation: "React Developer",
-    gender: "male",
-    teamName: "TeamB"
-  },
-  {
-    id: 5,
-    fullName: "David Henry",
-    designation: "DotNet Developer",
-    gender: "male",
-    teamName: "TeamB"
-  },
-  {
-    id: 6,
-    fullName: "Sarah Blake",
-    designation: "SQL Server DBA",
-    gender: "female",
-    teamName: "TeamB"
-  },
-  {
-    id: 7,
-    fullName: "James Bennet",
-    designation: "Angular Developer",
-    gender: "male",
-    teamName: "TeamC"
-  },
-  {
-    id: 8,
-    fullName: "Jessica Faye",
-    designation: "API Developer",
-    gender: "female",
-    teamName: "TeamC"
-  },
-  {
-    id: 9,
-    fullName: "Lita Stone",
-    designation: "C++ Developer",
-    gender: "female",
-    teamName: "TeamC"
-  },
-  {
-    id: 10,
-    fullName: "Daniel Young",
-    designation: "Python Developer",
-    gender: "male",
-    teamName: "TeamD"
-  },
-  {
-    id: 11,
-    fullName: "Adrian Jacobs",
-    designation: "Vue Developer",
-    gender: "male",
-    teamName: "TeamD"
-  },
-  {
-    id: 12,
-    fullName: "Devin Monroe",
-    designation: "Graphic Designer",
-    gender: "male",
-    teamName: "TeamD"
-  }])
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      fullName: "Bob Jones",
+      designation: "JavaScript Developer",
+      gender: "male",
+      teamName: "TeamA",
+    },
+    {
+      id: 2,
+      fullName: "Jill Bailey",
+      designation: "Node Developer",
+      gender: "female",
+      teamName: "TeamA",
+    },
+    {
+      id: 3,
+      fullName: "Gail Shepherd",
+      designation: "Java Developer",
+      gender: "female",
+      teamName: "TeamA",
+    },
+    {
+      id: 4,
+      fullName: "Sam Reynolds",
+      designation: "React Developer",
+      gender: "male",
+      teamName: "TeamB",
+    },
+    {
+      id: 5,
+      fullName: "David Henry",
+      designation: "DotNet Developer",
+      gender: "male",
+      teamName: "TeamB",
+    },
+    {
+      id: 6,
+      fullName: "Sarah Blake",
+      designation: "SQL Server DBA",
+      gender: "female",
+      teamName: "TeamB",
+    },
+    {
+      id: 7,
+      fullName: "James Bennet",
+      designation: "Angular Developer",
+      gender: "male",
+      teamName: "TeamC",
+    },
+    {
+      id: 8,
+      fullName: "Jessica Faye",
+      designation: "API Developer",
+      gender: "female",
+      teamName: "TeamC",
+    },
+    {
+      id: 9,
+      fullName: "Lita Stone",
+      designation: "C++ Developer",
+      gender: "female",
+      teamName: "TeamC",
+    },
+    {
+      id: 10,
+      fullName: "Daniel Young",
+      designation: "Python Developer",
+      gender: "male",
+      teamName: "TeamD",
+    },
+    {
+      id: 11,
+      fullName: "Adrian Jacobs",
+      designation: "Vue Developer",
+      gender: "male",
+      teamName: "TeamD",
+    },
+    {
+      id: 12,
+      fullName: "Devin Monroe",
+      designation: "Graphic Designer",
+      gender: "male",
+      teamName: "TeamD",
+    },
+  ]);
 
   return (
-    <main className='container' >
+    <main className="container">
+      {/* dropdown */}
+      <div className="row  justify-content-center mt-3 mb-3">
+        <div className="col-6">
+          <select name="team" id="team" className="form-select form-select-lg">
+            <option value="TeamA">TeamA</option>
+            <option value="TeamB">TeamB</option>
+            <option value="TeamC">TeamC</option>
+            <option value="TeamD">TeamD</option>
+          </select>
+        </div>
+      </div>
+
+      {/* cards */}
       <div className="row justify-content-center mt-3 mb-3">
         <div className="col-8">
           <div className="card-collection">
-            {employees.map(employee=>(
-              <div id={employee.id} className="card m-2" style={{cursor: 'pointer'}} >
-                {employee.gender === 'male'?<img src={maleProfile} className='card-img-top' alt="picture profile" />:
-                        <img src={femaleProfile} className='card-img-top' alt="picture profile" />}
+            {employees.map((employee) => (
+              <div
+                key={employee.id}
+                id={employee.id}
+                className="card m-2"
+                style={{ cursor: "pointer" }}
+              >
+                {employee.gender === "male" ? (
+                  <img
+                    src={maleProfile}
+                    className="card-img-top"
+                    alt="picture profile"
+                  />
+                ) : (
+                  <img
+                    src={femaleProfile}
+                    className="card-img-top"
+                    alt="picture profile"
+                  />
+                )}
 
                 <div className="card-body">
-                  <h5 className="card-title">Full name: {employee.fullName}
-                  </h5>
-                    <p className="card-text"><b>Designation:</b> {employee.designation}</p>
+                  <h5 className="card-title">Full name: {employee.fullName}</h5>
+                  <p className="card-text">
+                    <b>Designation:</b> {employee.designation}
+                  </p>
                 </div>
               </div>
             ))}
@@ -111,8 +141,7 @@ const Employees = () => {
         </div>
       </div>
     </main>
-  )
-}
+  );
+};
 
-
-export default Employees
+export default Employees;
