0a0602000f71e21856c835a2471fce481c64a5e5
khalidlotfi <khalidlotfi2005@gmail.com>
2024-04-08 12:05:58 +0000

    adding GroupedTeamMembers component

---
+++ src/components/GroupedTeamMembers.jsx
@@ -0,0 +1,15 @@
+const GroupedTeamMembers = ({ employee, selectedTeam, setTeam }) => {
+  return (
+    <header className="container">
+      <div className="row justify-content-center mt-3 mb-4 ">
+        <div className="col-8 ">
+          <h1 className="mt-4  bg-secondary text-white rounded">
+            Grouped Team Members
+          </h1>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default GroupedTeamMembers;
